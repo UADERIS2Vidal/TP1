b1a8bcfdb36f8aab7c55a1a5ee868e6ee98850b4
UADER_IS2_{tu_apellido} <stroktw7@gmail.com>
2025-03-26 06:04:06 -0300

    Se pide un argumento si no se pasa

--- src/factorial/factorial.py
+++ src/factorial/factorial.py
@@ -6,13 +6,13 @@
 #* Creative commons                                                        *
 #*-------------------------------------------------------------------------*
 import sys
+
 def factorial(num): 
     if num < 0: 
         print("Factorial de un número negativo no existe")
         return 0
     elif num == 0: 
         return 1
-        
     else: 
         fact = 1
         while(num > 1): 
@@ -20,9 +20,13 @@ def factorial(num):
             num -= 1
         return fact 
 
-if len(sys.argv) == 0:
-   print("Debe informar un número!")
-   sys.exit()
-num=int(sys.argv[1])
-print("Factorial ",num,"! es ", factorial(num)) 
+# Verificamos si se pasó un argumento
+if len(sys.argv) == 1:
+    # Si no se pasó argumento, solicitamos al usuario
+    num = int(input("Debe informar un número! Ingrese un número: "))
+else:
+    # Si se pasó argumento, usamos el argumento
+    num = int(sys.argv[1])
+
+print("Factorial ", num, "! es ", factorial(num)) 
 
